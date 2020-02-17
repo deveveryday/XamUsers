3542d5d20e23b45e5109da4ca026d903f1b30a56
Artur Justino <arturskateeveryday@gmail.com>
2020-02-17 10:23:34 -0300

    Adjusting app

---
+++ XamUsers/XamUsers.Android/obj/Debug/81/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 8C41967DC670C7DE */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1222
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 320165
+	.include	"typemap.mj.inc"
